2ed55ca6b6d652c76362cea1a10c61cdf3b82ef4
U1108743 <132808149+U1108743@users.noreply.github.com>
2023-05-07 17:47:01 +1000

    Update database_error.php

--- errors/database_error.php
+++ errors/database_error.php
@@ -1,5 +1,11 @@
-    <?php
+<?php include '../view/admin_manager/header_admin.php' ?>
+<header>
+<?php
         if (isset($e)) {
             echo $e;
         }
     ?>
+</header>
+<main>
+    echo "you should never see this"
+</main>
